f9fd56ad477b2cf9d631fdca12b06eba5f9a7a8b
Dakota Ryan Lambert <drlamb@umich.edu>
2018-07-05 14:49:50 -0400

    Implement basic system info display

--- Source/caen-help.py
+++ Source/caen-help.py
@@ -1,6 +1,8 @@
 import gi
 import sys
 import webbrowser
+import socket
+import uuid
 gi.require_version('Gtk', '3.0')
 from gi.repository import Gtk
 from gi.repository import Gio
@@ -8,7 +10,7 @@ from gi.repository import Gio
 class CaenHelp(Gtk.Application):
     def __init__(self):
         Gtk.Application.__init__(self)
-        
+
     def do_activate(self):
         # Configure the initial window
         window = Gtk.ApplicationWindow(application=self)
@@ -17,31 +19,38 @@ class CaenHelp(Gtk.Application):
         window.set_resizable(0)
         window.set_border_width(10)
 
-        # Establish a frame around the caen logo
-        logoframe = Gtk.Frame()
-        logoframe.set_shadow_type(0)
-        logo = Gtk.Image.new_from_file('/home/drlamb/git/caen-help/Source/caen.png')
-        logoframe.add(logo)
-
         # Create and define a Gtk.Grid
         grid = Gtk.Grid()
         grid.set_column_spacing(5)
         grid.set_row_spacing(5)
         grid.set_halign(1)
         grid.set_valign(1)
+
+        # Establish a frame around the caen logo
+        logoframe = Gtk.Frame()
+        logoframe.set_shadow_type(0)
+        logo = Gtk.Image.new_from_file('/home/drlamb/git/caen-help/Source/caen.png')
+        logoframe.add(logo)
         grid.attach(logoframe, 0, 0, 1, 1 )
+
+        # Info field
+        infoframe = Gtk.Frame()
+        test = Gtk.Label.new(self.sysinfo())
+        test.set_justify(0)
+        infoframe.add(test)
+        grid.attach(infoframe, 0, 1, 1, 1)
 
         # Report Button
         reportbutton = Gtk.Button()
         reportbutton.set_label("Report a Problem")
         reportbutton.connect("clicked", self.report_problem)
-        grid.attach(reportbutton, 0, 1, 1, 1)
+        grid.attach(reportbutton, 0, 2, 1, 1)
 
         # FAQ Button
         faqbutton = Gtk.Button()
         faqbutton.set_label("Visit the FAQ")
         faqbutton.connect("clicked", self.visit_faq)
-        grid.attach(faqbutton, 0, 2, 1, 1)
+        grid.attach(faqbutton, 0, 3, 1, 1)
 
         # Attach grid to window and tell window to display everything attached
         window.add(grid)
@@ -52,6 +61,14 @@ class CaenHelp(Gtk.Application):
 
     def report_problem(reportbutton, data=None):
         print(f"hello")
+
+    @staticmethod
+    def sysinfo():
+        hostname = socket.gethostname()
+        ip = socket.gethostbyname(hostname)
+        macaddr_hex = uuid.UUID(int=uuid.getnode()).hex[-12:]
+        mac = ':'.join(macaddr_hex[i:i + 2] for i in range(0, 11, 2))
+        return f'Hostname: {hostname.split(".")[0]} \nIP: {ip} \nMAC: {mac.upper()}'
 
 if __name__ == '__main__':
     app = CaenHelp()
