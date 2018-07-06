2cb3283bdddc042baefb317f2b069714b0f89adc
Dakota Ryan Lambert <drlamb@umich.edu>
2018-07-06 15:51:56 -0400

    Add inital report window

--- Source/caen-help.py
+++ Source/caen-help.py
@@ -35,20 +35,19 @@ class CaenHelp(Gtk.Application):
 
         # Info field
         infoframe = Gtk.Frame()
-        test = Gtk.Label.new(self.sysinfo())
+        infoframe.set_shadow_type(3)
+        test = Gtk.Label(self.sysinfo())
         test.set_justify(0)
         infoframe.add(test)
         grid.attach(infoframe, 0, 1, 1, 1)
 
         # Report Button
-        reportbutton = Gtk.Button()
-        reportbutton.set_label("Report a Problem")
+        reportbutton = Gtk.Button.new_with_label("Report a Problem")
         reportbutton.connect("clicked", self.report_problem)
         grid.attach(reportbutton, 0, 2, 1, 1)
 
         # FAQ Button
-        faqbutton = Gtk.Button()
-        faqbutton.set_label("Visit the FAQ")
+        faqbutton = Gtk.Button.new_with_label("Visit the FAQ")
         faqbutton.connect("clicked", self.visit_faq)
         grid.attach(faqbutton, 0, 3, 1, 1)
 
@@ -60,8 +59,41 @@ class CaenHelp(Gtk.Application):
         webbrowser.open('http://caenfaq.engin.umich.edu/')
 
     def report_problem(reportbutton, data=None):
-        print(f"hello")
+        window = Gtk.ApplicationWindow()
+        window.set_title("")
+        window.set_position(1)
+        window.set_resizable(0)
+        window.set_border_width(10)
+        grid = Gtk.Grid()
+        grid.set_column_spacing(5)
+        grid.set_row_spacing(5)
+        grid.set_halign(1)
+        grid.set_valign(1)
 
+        question1 = Gtk.Label("Is the problem with this computer?")
+        answer1 = Gtk.ComboBoxText()
+        answer1.append_text("Yes")
+        answer1.append_text("No")
+        answer1.set_active(0)
+
+        question2 = Gtk.Label("Please describe the issue:")
+        answer2 = Gtk.Entry()
+
+        question3 = Gtk.Label("Screenshot(s) of issue or other useful information:")
+
+
+        submit = Gtk.Button.new_with_label("Submit")
+
+        grid.attach(question1, 0, 0, 1, 1)
+        grid.attach(answer1, 1, 0, 1, 1)
+        grid.attach(question2, 0, 1, 1, 1)
+        grid.attach(answer2, 1, 1, 1, 1,)
+        grid.attach(question3, 0, 2, 1, 1)
+        grid.attach(submit, 1, 3, 1, 1)
+        window.add(grid)
+        window.show_all()
+
+        
     @staticmethod
     def sysinfo():
         hostname = socket.gethostname()
